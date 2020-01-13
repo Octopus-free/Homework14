f938c77718e05c9682f1884208275ecdcffa5536
Octopus-free <ivashkinrg@gmail.com>
2020-01-13 23:55:17 +0300

    Second

--- rbc_news_parser.py
+++ rbc_news_parser.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import re
 
 print('Данный парсер выводит статьи из раздела Газеты сайта rbc.ru')
-print('Список статей: ')
+
 
 url = 'https://rbc.ru'
 
@@ -41,7 +41,7 @@ for each_url in url_soup.find_all('a', class_ = 'newspaper-page__news'):
     if 'https' and 'newspaper' in str(each_url):
         newspapers_dict[' '.join(re.findall(r'\w+', each_url.get_text()))]  = each_url.get('href')
 #
-#pprint.pprint(newspapers_dict)
+# pprint.pprint(newspapers_dict)
 
 text_data_dict = {}
 
@@ -72,18 +72,40 @@ for each_url in newspapers_dict.values():
 
     text_data_dict[each_url]['text'] = text_data_dict[each_url]['text'].replace('\xa0', ' ')
 
-
-# print(len(text_data_dict.keys()))
-article_number = 0
+data_list = []
+print('Сейчас в разделе Газеты сайта rb.cru {} статей'.format(len(text_data_dict.keys())))
 for each_key in text_data_dict.keys():
-    article_number += 1
-    print (f'{article_number}. ', text_data_dict[each_key]['title'], f'.', f'Дата публикации -', text_data_dict[each_key]['data'])
+    if text_data_dict[each_key]['data'] not in data_list:
+        data_list.append(text_data_dict[each_key]['data'])
 
-article_for_read = int(input('Какую статью Вы хотели бы прочитать? '))
+unique_data_list = set(data_list)
 
-url_for_read = list(text_data_dict.keys())[article_for_read - 1]
+print('Статьи публиковались: ')
+for element in sorted(unique_data_list):
+    print(f'{sorted(unique_data_list).index(element) + 1}. ', element)
 
-print(newspapers_dict[url_for_read])
+data_choice = int(input('За какую дату вы хотели бы просмотреть список статей (введите номер)? '))
+
+data_for_read = list(unique_data_list)[data_choice - 1]
+
+print(f'Список статей за {data_for_read}:')
+
+article_list = []
+
+for each_key in text_data_dict.keys():
+    if text_data_dict[each_key]['data'] == data_for_read:
+        article_list.append(each_key)
+        list_number = article_list.index(each_key)
+        print (f'{list_number + 1}. ', text_data_dict[each_key]['title'], f'.')
+
+article_for_read = int(input(f'Какую статью за {data_for_read} Вы хотели бы прочитать? '))
+
+url_for_read = article_list[article_for_read - 1]
+
+print('Ссылка на статью на сайте: ')
+print(url_for_read)
+print('Текст статьи: ')
+pprint.pprint(text_data_dict[url_for_read]['text'])
 
 
 
